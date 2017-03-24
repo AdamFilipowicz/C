b4f579f877e1497aa55b4b1a9e4c7915f411e816
AdamFilipowicz <adam.adamon@gmail.com>
2017-03-24 18:42:45 +0100

    Update PPLista1Zad4.cpp

--- PPLista1Zad4.cpp
+++ PPLista1Zad4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #define MAX 3
-
+//Program z 3 podanych dat zwraca najwczeÅ›niejszÄ… z podanych
 using namespace std;
 
 struct Data{int dzien;int miesiac;int rok;};
@@ -31,7 +31,7 @@ int main(){
             if (tab[i-1].miesiac>0 && tab[i-1].miesiac<13){
                 if (tab[i-1].miesiac==1 || tab[i-1].miesiac==3 || tab[i-1].miesiac==5 || tab[i-1].miesiac==7 || tab[i-1].miesiac==8 || tab[i-1].miesiac==10 || tab[i-1].miesiac==12){
                     if (tab[i-1].dzien>0 && tab[i-1].dzien<32){
-                        printf("Najwczeœniejsza poprawna data to: %d.%d.%d",tab[i-1].dzien,tab[i-1].miesiac,tab[i-1].rok);
+                        printf("NajwczeÅ›niejsza poprawna data to: %d.%d.%d",tab[i-1].dzien,tab[i-1].miesiac,tab[i-1].rok);
                         return 1;
                     }
                 }
@@ -39,20 +39,20 @@ int main(){
                 else{
                     if (tab[i-1].miesiac==4 || tab[i-1].miesiac==6 || tab[i-1].miesiac==9 || tab[i-1].miesiac==11){
                         if (tab[i-1].dzien>0 && tab[i-1].dzien<31){
-                            printf("Najwczeœniejsza poprawna data to: %d.%d.%d",tab[i-1].dzien,tab[i-1].miesiac,tab[i-1].rok);
+                            printf("NajwczeÅ›niejsza poprawna data to: %d.%d.%d",tab[i-1].dzien,tab[i-1].miesiac,tab[i-1].rok);
                             return 1;
                         }
                     }
                     else{
                         if (tab[i-1].rok%4==00 && (tab[i-1].rok%100!=0 || tab[i-1].rok%400==0)){
                                 if (tab[i-1].dzien>0 && tab[i-1].dzien<30){
-                                    printf("Najwczeœniejsza poprawna data to: %d.2.%d",tab[i-1].dzien,tab[i-1].rok);
+                                    printf("NajwczeÅ›niejsza poprawna data to: %d.2.%d",tab[i-1].dzien,tab[i-1].rok);
                                     return 1;
                                 }
                         }
                         else{
                             if (tab[i-1].dzien>0 && tab[i-1].dzien<29){
-                                printf("Najwczeœniejsza poprawna data to: %d.2.%d",tab[i-1].dzien,tab[i-1].rok);
+                                printf("NajwczeÅ›niejsza poprawna data to: %d.2.%d",tab[i-1].dzien,tab[i-1].rok);
                                 return 1;
                             }
                         }
